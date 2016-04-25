eea482fd25c3c58b26d0c962565bdef2f5a3f247
Raphael Deem <raphael.deem@gmail.com>
2016-04-24 18:00:08 -0700

    first test function; first commit from xcode

--- SwiftStatsTests/SwiftStatsTests.swift
+++ SwiftStatsTests/SwiftStatsTests.swift
@@ -21,9 +21,9 @@ class SwiftStatsTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testBernoulli() {
         var b = SwiftStats.Distributions.Bernoulli(p: 0.7)
-        print(b.Pmf(1)) // 0.7
+        XCTAssert(b.Pmf(1)==0.7, "Bernoulli Pmf(1) should be 0.7")
         print(b.Cdf(1)) // 1.0
         print(b.Cdf(0)) // 0.3
         print(b.Quantile(0.5)) // 1
@@ -32,8 +32,8 @@ class SwiftStatsTests: XCTestCase {
         b = SwiftStats.Distributions.Bernoulli(data: [1,1,0,1])
         print(b.p)
         print(b.random())
-        
-        
+    }
+    func testLaplace(){
         var l = SwiftStats.Distributions.Laplace(mean: 0.0, b: 1.0)
         print(l.pdf(1))
         print(l.random())
@@ -42,7 +42,8 @@ class SwiftStatsTests: XCTestCase {
         
         l = SwiftStats.Distributions.Laplace(data:[12,13,12])
         print(l.mean)
-        
+    }
+    func testPoisson() {
         var p = SwiftStats.Distributions.Poisson(m: 1.5)
         print(p.Pmf(3))
         print(p.Cdf(1))
@@ -53,39 +54,39 @@ class SwiftStatsTests: XCTestCase {
         p = SwiftStats.Distributions.Poisson(data: [1,2,3])
         print(p.m)
         print(p.Quantile(0.9))
-        
+    }
+    func testGeometric() {
         let g = SwiftStats.Distributions.Geometric(p: 0.5)
         print(g.Pmf(3))
         print(g.Cdf(3))
         print(g.Cdf(4))
         print(g.Quantile(0.9999))
         print(g.random())
-        
+    }
+    func testExponential() {
         let e = SwiftStats.Distributions.Exponential(l: 0.5)
         print(e.pdf(3))
         print(e.Cdf(3))
         print(e.Cdf(4))
         print(e.Quantile(0.9999))
         print(e.random())
-
+    }
+    func testErfinv() {
         print(SwiftStats.Common.erfinv(erf(1.4)))
-        
-        
+    }
+    func testNormal() {
         var n = SwiftStats.Distributions.Normal(m: 0.0, v: 3)
         print(n.Cdf(pow(3,0.5))-n.Cdf(-pow(3,0.5)))
         print(n.Quantile(n.Cdf(3)))
         n = SwiftStats.Distributions.Normal(data: [1,2,1,0,1,2])
         print(n.v)
-        
+    }
+    func testUniform() {
         let u = SwiftStats.Distributions.Uniform(a:5,b:10)
         print(u.random())
-
-    
-        
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
+
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measureBlock {
