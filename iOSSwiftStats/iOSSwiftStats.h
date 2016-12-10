5d6f7762264c3f2a347804d471506c5755ff4fca
Raphael Deem <raphael.deem@gmail.com>
2016-12-10 13:46:50 -0800

    Add iOS target

---
+++ iOSSwiftStats/iOSSwiftStats.h
@@ -0,0 +1,13 @@
+//
+//  iOSSwiftStats.h
+//  iOSSwiftStats
+//
+//  Created by Raphael Deem on 12/10/16.
+//  Copyright © 2016 Raphael Deem. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSSwiftStats : NSObject
+
+@end
