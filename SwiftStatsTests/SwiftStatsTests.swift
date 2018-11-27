1127d823b59213c99447de5637a40cbc90fef5c8
Matthew Walker <mattgwwalker@gmail.com>
2018-11-27 13:06:40 +1300

    Removed unused performance test

--- SwiftStatsTests/SwiftStatsTests.swift
+++ SwiftStatsTests/SwiftStatsTests.swift
@@ -114,11 +114,4 @@ class SwiftStatsTests: XCTestCase {
         XCTAssert(u.Random())
     }
     */
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
